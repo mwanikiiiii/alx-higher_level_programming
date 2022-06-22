259aae02d877b3abec0f35861b61c769ff0cf050
mwanikiiiii <99424295+mwanikiiiii@users.noreply.github.com>
2022-06-22 04:00:35 +0300

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,31 +1,57 @@
 #!/usr/bin/python3
+"""
+Module 4-square
+Defines class Square with private size and public area
+Can access and update size
+"""
+
+
 class Square:
-    """Represents a square.
-    Private instance attribute: size:
-        - property def size(self)
-        - property setter def size(self, value)
-    Instantiation with optional size.
-    Public instance method: def area(self).
+    """
+    class Square definition
+    Args:
+        size (int): size of a side in square
+    Functions:
+        __init__(self, size)
+        size(self)
+        size(self, value)
+        area(self)
     """
 
     def __init__(self, size=0):
-        """Initializes the data."""
-        self.__size = size
+        """
+        Initializes square
+        Attributes:
+            size (int): defaults to 0 if none; don't use __size to call setter
+        """
+        self.size = size
 
     @property
     def size(self):
-        """Retrieves the size."""
+        """"
+        Getter
+        Return: size
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Sets the size to a value."""
-        if not isinstance(value, int):
+        """
+        Setter
+        Args:
+            value: sets size to value, if int and >= 0
+        """
+        if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        else:
+            self.__size = value
 
     def area(self):
-        """Returns the current square area."""
-        return self.__size ** 2
+        """
+        Calculates area of square
+        Returns:
+            area
+        """
+        return (self.__size)**2
