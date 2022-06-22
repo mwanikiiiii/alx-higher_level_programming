04f3fc218cfd388266f2cf4fef26cb62e9a40478
mwanikiiiii <99424295+mwanikiiiii@users.noreply.github.com>
2022-06-22 03:59:44 +0300

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,19 +1,37 @@
 #!/usr/bin/python3
+"""
+Module 3-square
+Defines class Square with private attribute size and public attribute area
+"""
+
+
 class Square:
-    """Represents a square.
-    Private instance attribute: size.
-    Instantiation with optional size.
-    Public instance method: def area(self).
+    """
+    class Square definition
+    Args:
+        size (int): size of a side in square
+    Functions:
+        __init__(self, size)
+        area(self)
     """
 
     def __init__(self, size=0):
-        """Initializes the data."""
-        if not isinstance(size, int):
+        """
+        Initializes square
+        Attributes:
+            __size (int): size of a side of square, defaults to 0 if none
+        """
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        else:
+            self.__size = size
 
     def area(self):
-        """Returns the current square area."""
-        return self.__size ** 2
+        """
+        Calculates area of square
+        Returns:
+            area
+        """
+        return (self.__size)**2
